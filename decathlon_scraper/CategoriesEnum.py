36e69b423720dbf0b1174b7a5a20c7c003c13769
FelixNeubauer <felix.neubauer@tech11.com>
2024-05-14 16:18:22 +0200

    add products EDA

--- decathlon_scraper/CategoriesEnum.py
+++ decathlon_scraper/CategoriesEnum.py
@@ -1,14 +1,19 @@
 from enum import Enum
 
 class Categories(Enum):
+
     CAMPING = ('camping', 'zelte')
+    FITNESS = ('fitness', 'fitnessgerate-fur-zuhause')
+    BIKES = ('fahrrad-welt', 'fahrrader')
+    BIKES_SALE = ('fahrrad-welt', 'fahrrad_sale')
+
+
+
+
     # WASSERSPORT_WELT = ('wassersport-welt', 'pools-und-wasserspielzeug')
     # STAND_UP_PADDLING = ('wassersport-welt', 'stand-up-paddle-sup')
     # BASKETBALL = ('basketball', 'basketballe-netballe')
     # PADEL_TENNIS = ('padel-tennis', 'padelausrustung')
-    FITNESS = ('fitness', 'fitnessgerate-fur-zuhause')
-    BIKES = ('fahrrad-welt', 'fahrrader')
-    BIKES_SALE = ('fahrrad-welt', 'fahrrad_sale')
     # WINTERSPORT = ('wintersport', 'skiausrustung')
     #https://www.decathlon.de/deals/sale-produkte/f-sport-group_fahrrad-city_fahrrad-mtb_fahrrad-rennrad_fahrrad-trekking-gravel-cross?storeid=0070048700487
 
